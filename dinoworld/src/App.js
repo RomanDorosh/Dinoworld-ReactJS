0d09ae757c0964254663e62bcb7f2e999d25bb75
RomanDorosh <roma.dorosh@gmail.com>
2021-01-14 12:29:11 +0100

    Created React app, installed bootstrap and sass packages

---
+++ dinoworld/src/App.js
@@ -0,0 +1,12 @@
+import './mysass.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-danger">Hello</button>
+      <h1>Hello Dino World</h1>
+    </div>
+  );
+}
+
+export default App;
