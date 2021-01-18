76da1d5950a1fb1d1da15bb7883b07fe595304f7
RomanDorosh <roma.dorosh@gmail.com>
2021-01-18 20:55:06 +0100

    Improved responsive design and created some dino cards

--- dinoworld/src/components/Navbar.js
+++ dinoworld/src/components/Navbar.js
@@ -1,50 +1,50 @@
 import * as FaIcons from "react-icons/fa";
 // import * as IconName from "react-icons/io";
-import { useState} from 'react';
-import { Link } from 'react-router-dom';
-import { SidebarData } from './SidebarData'
-import "./NavbarStyle.scss"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "./NavbarStyle.scss";
 
 function Navbar() {
-    //Declare a state of side bar and give to it value "false"
-    const [sidebar, setSidebar] = useState(false);
-    //Using useState we change the value of sidebar variable and can show sidebar an close it
-    const showsidebar = () => setSidebar(!sidebar);
-    return (
-        <>
-           <div className="navbar">
-
-               <Link to="#" className="menu-bars">
-                   <FaIcons.FaBars onMouseEnter={showsidebar}/>
-               </Link>
-               <div className="logo"><Link to="/"><span>dinoworld</span></Link></div>
-               <div className="logIn">
-                   <button>Log In</button>
-                   <button>Sign In</button>
-
-               </div>
-               </div> 
-               <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-                   <ul className="nav-menu-items" /*onClick={showsidebar}*/ onMouseLeave={showsidebar}>
-                       {/* <li className="navbar-toggle">
-                           <Link to="#" className="menu-bars close">
-                           <IconName.IoMdClose onClick={showsidebar}/>
-                           </Link>
-                       </li> */}
-                       {SidebarData.map((item, index) => {
-                           return (
-                               <li key={index} className={item.cName}>
-                                   <Link to={item.path}>
-                                       {item.icon}
-                                       <span>{item.title}</span>
-                                   </Link>
-                               </li>
-                           )
-                       })}
-                   </ul>
-               </nav>
-        </>
-    )
+  //Declare a state of side bar and give to it value "false"
+  const [sidebar, setSidebar] = useState(false);
+  //Using useState we change the value of sidebar variable and can show sidebar an close it
+  const showsidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars onMouseEnter={showsidebar} />
+        </Link>
+        <div className="logo">
+          <Link to="/">
+            <span>dinoworld</span>
+          </Link>
+        </div>
+        <div className="logIn">
+          <button>Log In</button>
+          <button>Sign In</button>
+        </div>
+      </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul
+          className="nav-menu-items"
+          onClick={showsidebar} /*</nav>onMouseLeave={showsidebar}*/
+        >
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- dinoworld/src/components/TopDinosComponent.js
+++ dinoworld/src/components/TopDinosComponent.js
@@ -1,11 +1,85 @@
-import "./TopDinosComponent.css"
-
+import "./TopDinosComponent.css";
 
 export default function TopDinos() {
+  const dinos = [
+    {
+      name: "T-Rex",
+      weight: 7500,
+      height: 5,
+      diet: "Carnivore",
+      topSpeed: 40,
+      length: 12,
+      period: "Cretaceous",
+      img:
+        "https://www.wennoanimal.com/uploads/animals/original/playCard_DinoCarni_QR_CO-15.jpg",
+    },
+    {
+      name: "Triceratops",
+      weight: 12000,
+      height: 5,
+      diet: "Herbivore",
+      topSpeed: 30,
+      length: 8,
+      period: "Cretaceous",
+      img:
+        "https://www.wennoanimal.com/uploads/animals/original/playCard_DinoHerbi_QR_CO-10.jpg",
+    },
+    {
+      name: "Velociraptop",
+      weight: 15,
+      height: 0.5,
+      diet: "Carnivore",
+      topSpeed: 64,
+      length: 2,
+      period: "Cretaceous",
+      img:
+        "https://www.wennoanimal.com/uploads/animals/original/playCard_DinoCarni_QR_CO-12.jpg",
+    },
+    {
+      name: "Stegosaurus",
+      weight: 7000,
+      height: 5,
+      diet: "Herbivore",
+      topSpeed: 7,
+      length: 9,
+      period: "Jurassic",
+      img:
+        "https://www.wennoanimal.com/uploads/animals/original/playCard_DinoHerbi_QR_CO-09.jpg",
+    },
+    {
+      name: "Spinosaurus",
+      weight: 8300,
+      height: 7,
+      diet: "Carnivore",
+      topSpeed: 18,
+      length: 15,
+      period: "Cretaceous",
+      img:
+        "https://www.wennoanimal.com/uploads/animals/original/playCard_DinoCarni_QR_CO-13.jpg",
+    },
+  ];
 
- 
-    return (
-        <div id="bg-img-top-dinos">
-        </div>
-    )
+  return (
+    <div id="bg-img-top-dinos">
+      <div className="grid-container">
+        {dinos.map((dino) => {
+          return <DinoCard key={dino.name} {...dino} />;
+        })}
+      </div>
+    </div>
+  );
 }
+
+const DinoCard = ({ name, weight, height, diet, topSpeed, period, img }) => {
+  return (
+    <div className="grid-card">
+      <img src={img} alt="" />
+      <h3>{name}</h3>
+      <h6>Weight: {weight}</h6>
+      <h6>Height: {height}</h6>
+      <h6>Diet: {diet}</h6>
+      <h6>Top Speed: {topSpeed}</h6>
+      <h6>Period: {period}</h6>
+    </div>
+  );
+};
